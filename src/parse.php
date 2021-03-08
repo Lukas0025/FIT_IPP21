12fbdcc2cfb4fdbb78f47d00565a83bfc68802d8
Lukáš Plevač <lukasplevac@gmail.com>
2021-03-08 22:34:39 +0000

    fix --help

--- src/parse.php
+++ src/parse.php
@@ -3,11 +3,19 @@
     include "xml.namespace.php";
     include "IPP21.spec.php";
 
-    if ($argc > 1) {
-        echo "IPP CODE 21 PARSER\n";
-        echo "\n";
-        echo "connect source to stdin and on stdout is XML output\n";
-        exit(0);
+    if (count($argv) == 2) {
+
+        if ($argv[1] == "--help")  {
+            echo "IPP CODE 21 PARSER\n";
+            echo "\n";
+            echo "connect source to stdin and on stdout is XML output\n";
+            exit(0);
+        }
+
+        exit(10);
+
+    } else if (count($argv) > 2) {
+        exit(10);
     }
 
     $parser = new parse('php://stdin', $IPP21_LANG);
